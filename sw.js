7b57c4098185e9925108f86bfdc7159b06b3f130
manu-mohan <manu.telstra@gmail.com>
2019-04-25 10:59:59 +0000

    Automated deployment to GitHub Pages: 8a7da7a0c5154b068e4e407de8ef50b161792092

--- sw.js
+++ sw.js
@@ -6,12 +6,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "b501bf04004ce07f8ae541bb9ba1d5dd"
   },
   {
+    "url": "/_nuxt/34f2dd8c2ecb7d5a4680.js",
+    "revision": "b89731870e2b1cdf62cb2d1fd73fa283"
+  },
+  {
     "url": "/_nuxt/35ffcfd35e3dd3d35909.js",
     "revision": "92d3f50f968aacf59de05612c7c6c1c9"
   },
   {
-    "url": "/_nuxt/a25792017d48e3c3a437.js",
-    "revision": "7e2bff232d23d9df32173263d872047c"
+    "url": "/_nuxt/3d5a062966b06589b4d9.js",
+    "revision": "7484b2b9cb69c3d9ed6bac25592053bf"
   },
   {
     "url": "/_nuxt/b95663f2f3c9949b66e8.js",
@@ -24,10 +28,6 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/fbd6980956f37ce3b508.js",
     "revision": "cd50bd07c1a9bd62a6ba955cc80c9224"
-  },
-  {
-    "url": "/_nuxt/fccaae60e8110d00eb1b.js",
-    "revision": "eb989d91e4cad64d589a27f289903c85"
   }
 ], {
   "cacheId": "manumohan.xyz",
